715a227d5e68d0be25b3eb9c7ace6b8463b45619
Dmytro Bogatov <dmytro@dbogatov.org>
2020-04-06 09:04:51 -0400

    Read inputs from python project, run real queries.

--- dp-oram/src/main.cpp
+++ dp-oram/src/main.cpp
@@ -15,18 +15,21 @@ using namespace DPORAM;
 namespace po = boost::program_options;
 
 po::variables_map setupArgs(int argc, char* argv[]);
-void query(PathORAM::ORAM* oram, BPlusTree::Tree* tree, bool query);
-vector<pair<number, bytes>> generateData();
-tuple<PathORAM::AbsStorageAdapter*, PathORAM::AbsPositionMapAdapter*, PathORAM::AbsStashAdapter*, PathORAM::ORAM*, BPlusTree::AbsStorageAdapter*, BPlusTree::Tree*> constructIndices(vector<pair<number, bytes>> data, bool generate);
+void query(PathORAM::ORAM* oram, BPlusTree::Tree* tree, vector<pair<number, number>> queries);
+tuple<vector<pair<number, bytes>>, vector<pair<number, bytes>>, vector<pair<number, number>>> generateData(bool read);
+tuple<PathORAM::AbsStorageAdapter*, PathORAM::AbsPositionMapAdapter*, PathORAM::AbsStashAdapter*, PathORAM::ORAM*, BPlusTree::AbsStorageAdapter*, BPlusTree::Tree*> constructIndices(vector<pair<number, bytes>> oramIndex, vector<pair<number, bytes>> treeIndex, bool generate);
+
+number salaryToNumber(string salary);
+double numberToSalary(number salary);
 
 void LOG(LOG_LEVEL level, string message);
 void LOG(LOG_LEVEL level, boost::format message);
 
-const auto COUNT			 = 1000uLL;
-const auto ORAM_BLOCK_SIZE	 = 256uLL;
-const auto ORAM_LOG_CAPACITY = 10uLL;
-const auto ORAM_Z			 = 3uLL;
-const auto TREE_BLOCK_SIZE	 = 64uLL;
+auto COUNT				   = 1000uLL;
+auto ORAM_BLOCK_SIZE	   = 256uLL;
+auto ORAM_LOG_CAPACITY	   = 10uLL;
+const auto ORAM_Z		   = 3uLL;
+const auto TREE_BLOCK_SIZE = 64uLL;
 
 const auto KEY_FILE			 = "key.bin";
 const auto TREE_FILE		 = "tree.bin";
@@ -34,13 +37,34 @@ const auto ORAM_STORAGE_FILE = "oram-storage.bin";
 const auto ORAM_MAP_FILE	 = "oram-map.bin";
 const auto ORAM_STASH_FILE	 = "oram-stash.bin";
 
+const auto DATA_FILE  = "../../experiments-scripts/scripts/data.csv";
+const auto QUERY_FILE = "../../experiments-scripts/scripts/query.csv";
+
 int main(int argc, char* argv[])
 {
 	auto vm = setupArgs(argc, argv);
 
-	auto data																= generateData();
-	auto [oramStorage, oramPositionMap, oramStash, oram, treeStorage, tree] = constructIndices(data, vm["generateIndices"].as<bool>());
-	query(oram, tree, vm["query"].as<bool>());
+	auto [oramIndex, treeIndex, queries] = generateData(vm["readInputs"].as<bool>());
+
+	COUNT = oramIndex.size();
+
+	vector<int> sizes;
+	sizes.resize(COUNT);
+	transform(oramIndex.begin(), oramIndex.end(), sizes.begin(), [](pair<number, bytes> val) { return val.second.size(); });
+	ORAM_BLOCK_SIZE = *max_element(sizes.begin(), sizes.end());
+
+	ORAM_LOG_CAPACITY = ceil(log2(COUNT)) + 1;
+
+	LOG(INFO, boost::format("COUNT = %1%") % COUNT);
+	LOG(INFO, boost::format("ORAM_BLOCK_SIZE = %1%") % ORAM_BLOCK_SIZE);
+	LOG(INFO, boost::format("ORAM_LOG_CAPACITY = %1%") % ORAM_LOG_CAPACITY);
+	LOG(INFO, boost::format("ORAM_Z = %1%") % ORAM_Z);
+	LOG(INFO, boost::format("TREE_BLOCK_SIZE = %1%") % TREE_BLOCK_SIZE);
+
+	auto [oramStorage, oramPositionMap, oramStash, oram, treeStorage, tree] = constructIndices(oramIndex, treeIndex, vm["generateIndices"].as<bool>());
+	query(oram, tree, queries);
+
+	LOG(INFO, "Complete!");
 
 	delete oramStorage;
 	delete oramPositionMap;
@@ -58,7 +82,7 @@ po::variables_map setupArgs(int argc, char* argv[])
 	po::options_description desc("range query processor");
 	desc.add_options()("help", "produce help message");
 	desc.add_options()("generateIndices", po::value<bool>()->default_value(true), "if set, will generate ORAM and tree indices, otherwise will read files");
-	desc.add_options()("query", po::value<bool>()->default_value(true), "if set, will run queries");
+	desc.add_options()("readInputs", po::value<bool>()->default_value(true), "if set, will read inputs from files");
 	desc.add_options()("verbosity", po::value<LOG_LEVEL>(&__logLevel)->default_value(INFO), "verbosity level to output");
 
 	po::variables_map vm;
@@ -74,34 +98,83 @@ po::variables_map setupArgs(int argc, char* argv[])
 	return vm;
 }
 
-void query(PathORAM::ORAM* oram, BPlusTree::Tree* tree, bool query)
+void query(PathORAM::ORAM* oram, BPlusTree::Tree* tree, vector<pair<number, number>> queries)
 {
-	if (query)
-	{
-		const auto QUERY = 5;
-		auto oramId		 = BPlusTree::numberFromBytes(tree->search(QUERY)[0]);
-		auto block		 = oram->get(oramId);
-		auto result		 = PathORAM::toText(block, ORAM_BLOCK_SIZE);
+	LOG(INFO, boost::format("Running %1% queries...") % queries.size());
 
-		LOG(INFO, boost::format("For query %1% the result is %2%") % QUERY % result);
-	}
-	else
+	for (auto query : queries)
 	{
-		LOG(INFO, "Query stage skipped...");
+		auto oramIds = tree->search(query.first, query.second);
+		auto count	 = 0;
+		for (auto oramId : oramIds)
+		{
+			auto block	= oram->get(BPlusTree::numberFromBytes(oramId));
+			auto result = PathORAM::toText(block, ORAM_BLOCK_SIZE);
+			count++;
+		}
+
+		LOG(TRACE, boost::format("For query {%1%, %2%} the result size is %3%") % numberToSalary(query.first) % numberToSalary(query.second) % count);
 	}
 }
 
-vector<pair<number, bytes>> generateData()
+tuple<vector<pair<number, bytes>>, vector<pair<number, bytes>>, vector<pair<number, number>>> generateData(bool read)
 {
 	LOG(INFO, "Constructing data set...");
 
-	vector<pair<number, bytes>> data;
-	for (number i = 0; i < COUNT; i++)
+	vector<pair<number, bytes>> oramIndex;
+	vector<pair<number, bytes>> treeIndex;
+	vector<pair<number, number>> queries;
+	if (read)
 	{
-		data.push_back({i, PathORAM::fromText(to_string(i), ORAM_BLOCK_SIZE)});
+		ifstream dataFile(DATA_FILE);
+
+		string line = "";
+		auto i		= 0;
+		while (getline(dataFile, line))
+		{
+			vector<string> record;
+			boost::algorithm::split(record, line, boost::is_any_of(","));
+			auto salary = salaryToNumber(record[7]);
+
+			LOG(TRACE, boost::format("Salary: %1%, data length: %2%") % numberToSalary(salary) % line.size());
+
+			oramIndex.push_back({i, PathORAM::fromText(line, ORAM_BLOCK_SIZE)});
+			treeIndex.push_back({salary, BPlusTree::bytesFromNumber(i)});
+			i++;
+		}
+		dataFile.close();
+
+		ifstream queryFile(QUERY_FILE);
+
+		line = "";
+		while (getline(queryFile, line))
+		{
+			vector<string> query;
+			boost::algorithm::split(query, line, boost::is_any_of(","));
+			auto left  = salaryToNumber(query[0]);
+			auto right = salaryToNumber(query[1]);
+
+			LOG(TRACE, boost::format("Query: {%1%, %2%}") % numberToSalary(left) % numberToSalary(right));
+
+			queries.push_back({left, right});
+		}
+		queryFile.close();
+	}
+	else
+	{
+		for (number i = 0; i < COUNT; i++)
+		{
+			oramIndex.push_back({i, PathORAM::fromText(to_string(i), ORAM_BLOCK_SIZE)});
+			treeIndex.push_back({salaryToNumber(to_string(i)), BPlusTree::bytesFromNumber(i)});
+		}
+
+		for (number i = 0; i < COUNT / 10; i++)
+		{
+			queries.push_back({salaryToNumber(to_string(8 * i + 3)), salaryToNumber(to_string(8 * i + 8))});
+		}
 	}
 
-	return data;
+	return {oramIndex, treeIndex, queries};
 }
 
 tuple<
@@ -111,7 +184,7 @@ tuple<
 	PathORAM::ORAM*,
 	BPlusTree::AbsStorageAdapter*,
 	BPlusTree::Tree*>
-constructIndices(vector<pair<number, bytes>> data, bool generate)
+constructIndices(vector<pair<number, bytes>> oramIndex, vector<pair<number, bytes>> treeIndex, bool generate)
 {
 	LOG(INFO,
 		generate ?
@@ -151,30 +224,27 @@ constructIndices(vector<pair<number, bytes>> data, bool generate)
 
 	if (generate)
 	{
-		oram->load(data);
+		oram->load(oramIndex);
 		oramPositionMap->storeToFile(ORAM_MAP_FILE);
 		oramStash->storeToFile(ORAM_STASH_FILE);
 	}
 
-	auto treeStorage = new BPlusTree::FileSystemStorageAdapter(TREE_BLOCK_SIZE, TREE_FILE, generate);
-	BPlusTree::Tree* tree;
-
-	if (generate)
-	{
-		vector<pair<number, bytes>> index;
-		for (auto record : data)
-		{
-			index.push_back({record.first, BPlusTree::bytesFromNumber(record.first)});
-		}
-
-		tree = new BPlusTree::Tree(treeStorage, index);
-	}
-	else
-	{
-		tree = new BPlusTree::Tree(treeStorage);
-	}
+	auto treeStorage	  = new BPlusTree::FileSystemStorageAdapter(TREE_BLOCK_SIZE, TREE_FILE, generate);
+	BPlusTree::Tree* tree = generate ? new BPlusTree::Tree(treeStorage, treeIndex) : new BPlusTree::Tree(treeStorage);
 
 	return {oramStorage, oramPositionMap, oramStash, oram, treeStorage, tree};
+}
+
+number salaryToNumber(string salary)
+{
+	auto salaryDouble = stod(salary) * 100;
+	auto salaryNumber = (long long)salaryDouble + (LLONG_MAX / 4);
+	return (number)salaryNumber;
+}
+
+double numberToSalary(number salary)
+{
+	return ((long long)salary - (LLONG_MAX / 4)) * 0.01;
 }
 
 void LOG(LOG_LEVEL level, boost::format message)
