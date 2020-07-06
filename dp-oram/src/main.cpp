47813080fbf1c3a4d57501f0455b64ef692e12df
Dmytro Bogatov <dmytro@dbogatov.org>
2020-07-06 00:02:49 -0400

    Fix.

--- dp-oram/src/main.cpp
+++ dp-oram/src/main.cpp
@@ -979,13 +979,13 @@ int main(int argc, char* argv[])
 				printProfileStats(profiles);
 			}
 
+			queryIndex++;
+
 			if (SIGINT_RECEIVED)
 			{
 				LOG(WARNING, L"Stopping query processing due to SIGINT");
 				break;
 			}
-
-			queryIndex++;
 		}
 
 		if (!VIRTUAL_REQUESTS && rpcClients.size() == 0)
@@ -1162,7 +1162,7 @@ int main(int argc, char* argv[])
 #pragma region WRITE_JSON
 
 	LOG(INFO, boost::wformat(L"For %1% queries: total: %2%, average: %3% / query, fastest thread: %4% / query, %5% / fetched item; (%6%+%7%+%8%=%9%) records / query") % (queryIndex - 1) % timeToString(timeTotal) % timeToString(timePerQuery) % timeToString(fastestThreadPerQuery) % timeToString(timeTotal / totalTotal) % realPerQuery % paddingPerQuery % noisePerQuery % totalPerQuery);
-	LOG(INFO, boost::wformat(L"For %1% queries: ingress traffic: %2% (%3% / query), egress traffic: %4% (%5% per query)") % (queryIndex - 1) % bytesToString(ingress) % bytesToString(ingress / (queryIndex - 1)) % bytesToString(egress) % bytesToString(egress / (queryIndex - 1)));
+	LOG(INFO, boost::wformat(L"For %1% queries: ingress traffic: %2% (%3% / query), egress traffic: %4% (%5% per query), network usage per query: %6%, or %3i% of DB size") % (queryIndex - 1) % bytesToString(ingress) % bytesToString(ingress / (queryIndex - 1)) % bytesToString(egress) % bytesToString(egress / (queryIndex - 1)) % bytesToString((ingress + egress) / (queryIndex - 1)) % (100 * (ingress + egress) / (COUNT * ORAM_BLOCK_SIZE)));
 	if (PROFILE_STORAGE_REQUESTS)
 	{
 		printProfileStats(allProfiles, queryIndex - 1);
